f9cc52f91c916298fefe64638a0979a01bf6c34c
Linnea <linneagear@gmail.com>
2020-10-07 11:00:24 -0700

    Added package.json and appropriate dependencies

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { useHistory } from "react-router-dom";
+import API from "../../../utils/API";
+
+function log() {
+
+  // useState to grab the email and password from the form
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // capture email and password values —————————————————|
+  const userEmailValue = (event) => {
+    setEmail(event.target.value);
+  };
+    
+  const userPasswordValue = (event) => {
+    setPassword(event.target.value);
+  };
 
 
-function  log() {
+
     return (
         <div>
             
