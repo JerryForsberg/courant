da72b548e7f1da899cb869a0976a8908fbe9429c
JerryForsberg <Jerryforsberg8991@gmail.com>
2020-10-07 22:51:28 -0700

    worked on some models and some file structure for the client side

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    User: require("./user"),
+    Story: require("./stories")
+};
--- src/utils/API.js
+++
@@ -1,6 +0,0 @@
-// this is where all of our API calls go, to connect from back end to front end
-import axios from "axios";
-
-export default {
-
-}
