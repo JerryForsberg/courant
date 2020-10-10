459c24816546397278926ce6972f0363eac3dd7f
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-10-10 15:48:27 -0700

    adding sidebar component

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -6,34 +6,32 @@ import { Link } from "react-router-dom";
 function nav() {
     return (
         <div className="container-fluid">
-            <div className="row mt-5 mb-5">
-                <div className="nav">
-                    <div className="navLinks">
-                        <div >
-                            <Link className="navlink" to="/discover">
-                                DISCOVER
+            <div className="nav mx-auto mt-2 mb-5">
+                <div className="row navLinks">
+                    <div >
+                        <Link className="navlink" to="/discover">
+                            DISCOVER
                             </Link>
-                        </div>
-                        <div >
-                            <Link className="navlink" to="/about">
-                                ABOUT
-                            </Link>
-                        </div>
                     </div>
-                    <div className="text-center">
-                        <img src="../images/logo.svg" alt="logo" className="login-logo" />
+                    <div >
+                        <Link className="navlink" to="/about">
+                            ABOUT
+                            </Link>
                     </div>
-                    <div className="logins">
-                        <div >
-                            <Link className="navlink"to="/login">
-                                SIGN IN
+                </div>
+                <div className="text-center">
+                    <img src="../images/logo.svg" alt="logo" className="login-logo" />
+                </div>
+                <div className="row logins">
+                    <div >
+                        <Link className="navlink" to="/login">
+                            SIGN IN
                             </Link>
-                        </div>
-                        <div >
-                            <Link className="navlink" to="/signup">
-                                SIGN UP
+                    </div>
+                    <div >
+                        <Link className="navlink" to="/signup">
+                            SIGN UP
                             </Link>
-                        </div>
                     </div>
                 </div>
             </div>
---
+++ client/src/components/sidenav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+
+function nav() {
+    return (
+        <div className="container-fluid">
+            
+        </div>
+    );
+}
+
+export default nav;
