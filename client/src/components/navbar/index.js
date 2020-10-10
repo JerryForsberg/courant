643c43b746ae0c369b744e9b5f739bef08575de1
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-10-10 12:19:20 -0700

    creating and styling main navbar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,10 +1,42 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
-function  nav() {
+
+function nav() {
     return (
-        <div>
-            
+        <div className="container-fluid">
+            <div className="row mt-5 mb-5">
+                <div className="nav">
+                    <div className="navLinks">
+                        <div >
+                            <Link className="navlink" to="/discover">
+                                DISCOVER
+                            </Link>
+                        </div>
+                        <div >
+                            <Link className="navlink" to="/about">
+                                ABOUT
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <img src="../images/logo.svg" alt="logo" className="login-logo" />
+                    </div>
+                    <div className="logins">
+                        <div >
+                            <Link className="navlink"to="/login">
+                                SIGN IN
+                            </Link>
+                        </div>
+                        <div >
+                            <Link className="navlink" to="/signup">
+                                SIGN UP
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
