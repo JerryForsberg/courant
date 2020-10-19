ad417f11cbaeff1fbc286c66ce1f987c4f7a1410
Joey Reyes <joeyr1984@gmail.com>
2020-10-18 18:10:01 -0700

    added button component and styling

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -3,11 +3,8 @@ import "./style.css";
 
 function profile () {
     return (
-        <div className="container-fluid">
-            <img href=""/>
-            <p>
-                Placeholder for profile stuff maybe shows your posts
-            </p>
+        <div className="col-8 content-area">
+say cheese
         </div>
     );
 }
