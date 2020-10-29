5c27aa72dc273c212b5e81344b40994a42851c54
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-10-28 19:58:29 -0700

    rm package-lock.json

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function profile () {
     return (
         <div className="col-8 content-area">
-
+            <div>
+                {/* map through works from db call props */}
+                <Link to="/">
+                <img />
+                </Link>
+            </div>
         </div>
     );
 }
