88b661ab04e93202e4807780d1217c1b43e05440
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-10-30 14:44:54 -0700

    maping through db for stories

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import API from "../../utils/API";
+import StateManager from "react-select";
 
-function profile () {
+function profile() {
+    const [state, setState] = useState([]);
+
+    const getAllStories = () => {
+        API.getAllStories()
+            .then(res => {
+                setState(res.data);
+            }).catch(err => {
+                console.log(err);
+            }
+            )
+    }
+
+    useEffect(() => {
+        getAllStories();
+    }, []);
+
     return (
         <div className="col-8 content-area">
             <div>
-                {/* map through works from db call props
-                <Link to="/">
-                <img />
-                </Link> */}
+                {/* map through works from db call props */}
+                {state.map((data) => (
+            <Link to={"/api/story/" + data.id}>
+            <img src={data.imageUpload}>
+            </img>
+            </Link>
+            ))}
             </div>
         </div>
     );
