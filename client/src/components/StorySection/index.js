6748e72fffb59bac2513d1d8147201dd2aececdf
Linnea <linneagear@gmail.com>
2020-11-20 13:34:38 -0700

    Adding get stories functionality to profiles page

--- client/src/components/StorySection/index.js
+++ client/src/components/StorySection/index.js
@@ -1,56 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 import "./style.css";
 import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
 import { useHistory, useParams } from "react-router-dom";
-import {useCourantContext} from "../../utils/CourantContext";
+import { useCourantContext } from "../../utils/CourantContext";
 
-function StoriesCard(props) {
-    const {logout } = useCourantContext();
-    const [story, setStory] = useState({})
-    const {id} = useParams();
+function StoryCard(props) {
+  const { logout } = useCourantContext();
+  const [story, setStory] = useState({})
+  const { id } = useParams();
 
-    useEffect(() => {
-      API.getBook(id)
-        .then(res => setBook(res.data))
-        .catch(err => console.log(err));
-    }, [])
+  const history = useHistory();
 
-    const getLatestStories = () => {
-        API.findAllStories()
-        .then((res) => {
-          if (res.data.isAuthenticated === false) {
-            return logout(history);
-          }
-          if (res.data.length === 0) {
-            history.push("/profile");
-          }
-          setStoryInfo(res.data);
-        })
-          .catch((err) => console.log(err));
-      }
+  // When this component mounts, grab the book with the _id of props.match.params.id
+  useEffect(() => {
+    API.getStory(id)
+      .then(res => setStory(res.data))
+      .catch(err => console.log(err));
+  }, [])
 
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <h1>
+            {story.title} by {story.author}
+          </h1>
+        </Col>
+      </Row>
 
-
-    // option to delete story
-    const submitDeleteStory = () => {
-        API.deleteStory(props.storyID)
-        .then((response) => {
-            props.getLatestStories();
-        })
-        .catch((error) => {
-            console.log(error);
-          });
-    }
-
-    return (
-
-        <div>
-            <h2>Delete {props.title}</h2>
-            <p >Are you sure you want to permanently delete this story? This action can not be undone!</p>
-            <button onClick={submitDeleteStory}>Yes, Delete story</button>
-        </div>
-    )
+      <Row>
+        <Col size="md-10 md-offset-1">
+          <article>
+            <p>
+              {story.textUpload}
+            </p>
+          </article>
+        </Col>
+      </Row>
+    </Container>
+  )
 
 }
 
@@ -60,39 +50,39 @@ function StoriesCard(props) {
 // Stories Card ==================================== |
 // This is where the stories wil display
 function StorySection() {
-    const [stories, setStories] = useState({});
+  const [stories, setStories] = useState({});
 
-    // Loads all stories and sets them to stories
-    useEffect(() => {
-        API.findAllStories()
-        .then(res => 
-            setStories(res.data)
-        )
-            .catch(err => console.log(err)); 
-    }, []);
+  // Loads all stories and sets them to stories
+  useEffect(() => {
+    API.findAllStories()
+      .then(res =>
+        setStories(res.data)
+      )
+      .catch(err => console.log(err));
+  }, []);
 
 
-    return (
-        <div>
-            {/* populated stories will go in here */}
+  return (
+    <div>
+      {/* populated stories will go in here */}
 
-            {stories.length ? (
-                <li>
-                {stories.map(story => (
-                  <ul key={story._id}>
-                    <Link to={"/story/" + story._id}>
-                      <strong>
-                        {story.title} by {story.author}
-                      </strong>
-                    </Link>
-                  </ul>
-                ))}
-              </li>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-        </div>
-    )
+      {stories.length ? (
+        <li>
+          {stories.map(story => (
+            <ul key={story._id}>
+              <Link to={"/story/" + story._id}>
+                <strong>
+                  {story.title} by {story.author}
+                </strong>
+              </Link>
+            </ul>
+          ))}
+        </li>
+      ) : (
+          <h3>No Results to Display</h3>
+        )}
+    </div>
+  )
 }
 
 export default StorySection;
