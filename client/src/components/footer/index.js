0eba47cb0ee286e6d37c832759f28ec6d59662d3
Joey Reyes <joeyr1984@gmail.com>
2020-12-02 10:39:33 -0700

    fixes to footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -5,19 +5,41 @@ import "./style.css";
 function footer() {
     return (
         <>
-                <div className="keyboard-footer">
-                    <img src="./images/footer-banner.svg" alt="close up of typewriter keyboard" className="img-fluid"></img>
-                </div>
+            <div className="keyboard-footer">
+                <img src="./images/footer-banner.svg" alt="close up of typewriter keyboard" className="img-fluid"></img>
+            </div>
             <div className="footer-container">
+                <div className="row">
+                <div className="col-3">
+                    <img src="./images/logo-white.svg" alt="logo for Courant" className="logo-footer"></img>
+                </div>
 
-                <img src="./images/logo-white.svg" alt="logo for Courant" className="logo-footer"></img>
-                <section className="footer-links">
-                    <div className="footer-links-wrapper">
-                        <div className="footer-links-items">
-
-                        </div>
+                    <div className=" col-3 footer-links-items">
+                        <h1 className="footer-header">Site Map</h1>
+                        <h1 className="footer-header">Contact</h1>
+                        <h1 className="footer-header">Terms and Conditions</h1>
+                        <h1 className="footer-header">Careers</h1>
+                        <h1 className="footer-header">Help</h1>
                     </div>
-                </section>
+
+                    <div className=" col-3 footer-links-items">
+                        <h1 className="footer-header">Discover</h1>
+                        <h1 className="footer-header">Sign in</h1>
+                        <h1 className="footer-header">Login</h1>
+                        <h1 className="footer-header">About</h1>
+                        <h1 className="footer-header">Sponsers</h1>
+                    </div>
+
+                    <div className=" col-3 footer-links-items">
+                    <i className="fab fa-twitter footer-icons"></i>
+                    <i className="fab fa-instagram footer-icons"></i>
+                    <i className="fab fa-youtube footer-icons"></i>
+                    <i className="fab fa-facebook-square footer-icons"></i>
+
+
+                    </div>
+
+                    </div>
             </div>
 
             {/* 
